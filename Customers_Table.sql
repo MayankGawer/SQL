cfcb6bbd44e67c6496a405d9d1d7c0a1a00218ba
MayankGawer <93132274+MayankGawer@users.noreply.github.com>
2023-05-10 09:27:44 +1000

    Update Customers_Table.sql

---
+++ Customers_Table.sql
@@ -0,0 +1,9 @@
+Use sales;
+Create Table customers 
+(
+ customer_id INT Not null primary key,
+ first_name varchar(255) Not null,
+ last_name Varchar (255) Not null,
+ email_address Varchar (255) Not null,
+ number_of_complaints INT
+);
